6e98c15614945b242aa0913d5ac29989570d8d39
JosueTavaress <josueferreira8.jf@gmail.com>
2022-02-28 03:06:30 -0300

    feat: add middleware generate token

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,14 +2,16 @@ const service = require('../services/userService');
 const validateReq = require('../error/joiError');
 const schemaUser = require('../schemas/userSchema');
 
-const addUser = async (req, res) => {
+const addUser = async (req, _res, next) => {
   const dataUser = req.body;
 
   validateReq(schemaUser, dataUser);
 
   const newUser = await service.addUser(dataUser);
 
-  res.status(201).json({ message: newUser });
+  req.body = newUser
+
+  next();
 }
 
 module.exports = { addUser };
--- backend/src/routers/signupUserRouter.js
+++ backend/src/routers/signupUserRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const controller = require('../controllers/userController')
+const controller = require('../controllers/userController');
+const { generateAuth } = require('../middlewares/generateAuth');
 const user = express.Router();
 const rescue = require('express-rescue');
 
-user.post('/', rescue(controller.addUser));
+user.post('/', rescue(controller.addUser), generateAuth);
 
 module.exports = user;

7634a3391e061b12395e15da7f0cc70431ede899
JosueTavaress <josueferreira8.jf@gmail.com>
2022-02-28 02:30:48 -0300

    feat: add socket io statusCode/http

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,4 @@
-const express = require('express');
-const routerNewUser = require('./routers/signupUserRouter');
-const erroJoi = require('./middlewares/middlewareErroJoi');
-const domainErro = require('./middlewares/middlewareDomainErro');
-const badyParse = require('body-parser');
+const { server, PORT } = require('./http');
+require('./socketIo/webSocket');
 
-const PORT = process.env.PORT || '5050';
-
-const app = express();
-
-app.use(badyParse.json());
-
-app.use('/signup', routerNewUser);
-app.use(erroJoi);
-app.use(domainErro);
-
-app.listen(PORT, console.log(`online port ${PORT}`));
+server.listen(PORT, console.log(`online port ${PORT}`));

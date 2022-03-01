1593091616d4b05ef3c1c31d2f9276ab8e7a995a
JosueTavaress <josueferreira8.jf@gmail.com>
2022-02-28 21:31:49 -0300

    feat: implements chanels

--- backend/src/http.js
+++ backend/src/http.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 const routerNewUser = require('./routers/signupUserRouter');
 const routerLoginUser = require('./routers/loginUserRouter');
+const routerChanels = require('./routers/chanelRouter');
 const erroJoi = require('./middlewares/middlewareErroJoi');
 const domainErro = require('./middlewares/middlewareDomainErro');
 const badyParse = require('body-parser');
@@ -12,9 +14,11 @@ const PORT = process.env.PORT || '5050';
 const app = express();
 
 app.use(badyParse.json());
+app.use(cors());
 
 app.use('/signup', routerNewUser);
 app.use('/login', routerLoginUser);
+app.use('/chanels', routerChanels);
 app.use(erroJoi);
 app.use(domainErro);
 

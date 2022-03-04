b7661ae804b3ce7dacaec22fe757b6ddb8bbd117
JosueTavaress <josueferreira8.jf@gmail.com>
2022-03-04 18:02:05 -0300

    feat: config cors server socket

--- backend/src/http.js
+++ backend/src/http.js
@@ -18,7 +18,12 @@ app.use(badyParse.json());
 app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5000",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use('/signup', routerNewUser);
 app.use('/login', routerLoginUser);

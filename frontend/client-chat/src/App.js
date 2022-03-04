d2769c48f2d0af701d7d47ae64b1962f4c6e0bde
JosueTavaress <josueferreira8.jf@gmail.com>
2022-03-04 19:11:51 -0300

    feat: add router react

--- frontend/client-chat/src/App.js
+++ frontend/client-chat/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProviderContext from "./context/provider";
+import Chat from "./pages/chat";
+import SubUser from './pages/subscribe/subscribe'
+import Login from "./pages/singin";
+import SingUp from "./pages/singup";
+import Channels from "./pages/channel";
+
 function App() {
   return (
-    <div className="App">
-     <h1>chat</h1>
-    </div>
+  <ProviderContext>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/singup" element={<SingUp />} />
+        <Route path="/channels" element={<Channels />} />
+        <Route path="/chat/:id" element={<Chat />} />
+        <Route path="/subscribe" element={<SubUser/>} />
+      </Routes>
+     </BrowserRouter>
+  </ProviderContext>
   );
 }
 

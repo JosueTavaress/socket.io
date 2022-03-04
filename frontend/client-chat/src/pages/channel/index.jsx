07a033b4937dfb54876ca8b18ac29890931c3acb
JosueTavaress <josueferreira8.jf@gmail.com>
2022-03-04 19:25:14 -0300

    feat: add page channel

---
+++ frontend/client-chat/src/pages/channel/index.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { getChannels } from '../../service/requestApi';
+import {  GetLocalStorage } from '../../service/localStorage'
+import BtnRooms from '../../components/buttonRoom';
+
+const Channels = () => {
+    const [channels, setChannels] = useState([]);
+    const [load, setLoad] = useState(true);
+    const [token, setToken] = useState('');
+      
+      useEffect(() => {
+        setToken(GetLocalStorage('token'));
+      }, []);
+
+      useEffect(() => {
+         setTimeout(() => {
+          const fechtApi = async () => {
+            setChannels(await getChannels(token));
+            setLoad(false);
+           }
+           fechtApi();
+         }, 2000);
+
+      },[token]);
+
+  if (load) return <div>CARREGANDO...</div> // componentizar
+  if (!token) return <div>user não autorizado é preciso si identificar</div> // componentizar
+  if (!channels.length) return <div>não existe canais</div> // componentizar
+
+  return (
+    <section className='section'>
+    {channels.map((el, index) => <BtnRooms key={index} token={token} props={el}/>)}
+    </section>
+    );
+};
+
+export default Channels;
